d3435f20b8b234345a15d716be9574384738d9ad
DeepakTiwariPC <ainutiwari483@gmail.com>
2023-05-17 11:27:29 +0530

    added addition and equalPartition

---
+++ addition.js
@@ -0,0 +1,7 @@
+function addNumber(n1, n2)
+{
+  return n1 + n2
+}
+// const result = addNumber(1, 4);
+// console.log(result);
+console.log(addNumber(1, 3));
---
+++ equalPartition.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number} n
+ * @param {number[]} arr
+ * @return {number}
+ */
+function equalPartition(n, arr) {
+}
+//first find the prefix of (i-1) and find the 
